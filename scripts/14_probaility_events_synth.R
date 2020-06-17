ec3b26a3d2186f61e3ad37a02f01501935d08202
ksirving <32795054+ksirving@users.noreply.github.com>
2020-06-16 18:49:21 -0700

    figure for events under o.2 prob done

--- scripts/14_probaility_events_synth.R
+++ scripts/14_probaility_events_synth.R
@@ -7,7 +7,7 @@ library(gridExtra) # tile several plots next to each other
 library(scales)
 library(tidyr)
 library(tidyverse)
-install.packages("data.table")
+# install.packages("data.table")
 library(data.table)
 
 setwd("/Users/katieirving/Documents/git/flow_eco_mech")
@@ -24,38 +24,75 @@ head(new_data)
 # same with highs?
 
 ## less than 0.20 
-## groups a low probability event and counts the consecutuve hours
+## groups a low probability event and counts the consecutuve hours, plus add a column with month and year
 new_data <- new_data %>%
   group_by(ID = data.table::rleid(prob_fit < 0.2)) %>%
-  mutate(Consec_Hours = if_else(prob_fit < 0.2, row_number(), 0L))
+  mutate(Consec_Hours = if_else(prob_fit < 0.2, row_number(), 0L)) 
 
-## groups data by year and ID and reports maximum number of consecutive hours
+# %>%
+#   unite(month_year, month:year, remove=F)
+
+## groups data by year, month and ID and reports maximum number of consecutive hours
 conseq_ID20 <- new_data %>% 
   group_by(ID, month, year) %>%
   summarise(max(Consec_Hours)) %>%
   rename(max_Consec_Hours=`max(Consec_Hours)`) %>%
   mutate(max_Consec_Hours/24) %>%
-  rename(max_Consec_Days=`max_Consec_Hours/24`)
+  rename(max_Consec_Days=`max_Consec_Hours/24`) 
 
-conseq_ID20$max_Consec_Days[945:length(conseq_ID20$max_Consec_Days)]
+# conseq_ID20$max_Consec_Days[945:length(conseq_ID20$max_Consec_Days)]
+
+# no of days/hours under low prob
+
+## count number of events - below 20% and for 1 day - per month as well as number of times per month (hours) that the prob goes below zero
+consec_days_1 <- conseq_ID20 %>%
+  group_by(month, year) %>%
+  select(-max_Consec_Hours) %>%
+  summarize(n_times_lower_0.2 = n(),
+            n_times_grtr_1_day = sum(max_Consec_Days >= 1)) %>%
+  arrange(year)
+
+# create year_month column       
+consec_days_1 <- ungroup(consec_days_1) %>%
+  unite(month_year, year:month, sep="-", remove=F) 
+
+## convert month year to dat format
+consec_days_1$month_year <-  zoo::as.yearmon(consec_days_1$month_year)
+
+##  plot - number of days - this works!
+
+    ggplot(consec_days_1, aes(x =month_year, y=n_times_grtr_1_day)) +
+    geom_line(aes( y = n_times_grtr_1_day), color = "darkblue") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 1)) +
+    scale_x_continuous(breaks=as.numeric(consec_days_1$month_year), labels=format(consec_days_1$month_year,"%b %Y")) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    labs(title = "Suitability Events",
+         y = "Number of days per Month",
+         x = "Month") #+ theme_bw(base_size = 15)
+    
+
+  ##  plot - number of hours
+    ggplot(consec_days_1, aes(x =month_year, y=n_times_lower_0.2)) +
+      geom_line(aes( y = n_times_lower_0.2), color = "red") +
+      theme(axis.text.x = element_text(angle = 90, vjust = 1)) +
+      scale_x_continuous(breaks=as.numeric(consec_days_1$month_year), labels=format(consec_days_1$month_year,"%b %Y")) +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      labs(title = "Suitability Events",
+           y = "Number of hours per Month",
+           x = "Month") #+ theme_bw(base_size = 15)
+  
+
+
+## will have different definitions of "an event"
+## below 20% for what number of days
+## below 30% for same duration
+## or no of days/hours under low prob
+## try both 
 
 ## next steps
-## add days for each month together
-## want number of days, not consec days - or both??
 ## do for other %s
+## do for velocity/temoperature
 ## find change point/sweet spot/break point
+## send to all
 
-
-
-# 
-# DT = data.table(grp=rep(c("A", "B", "C", "A", "B"), c(2,2,3,1,2)), value=1:10)
-# DT
-# rleid(DT$grp) # get run-length ids
-# rleidv(DT, "grp") # same as above
-# 
-# rleid(DT$grp, prefix="grp") # prefix with 'grp'
-# 
-# # get sum of value over run-length groups
-# DT[, sum(value), by=.(grp, rleid(grp))]
-# DT[, sum(value), by=.(grp, rleid(grp, prefix="grp"))]
 
